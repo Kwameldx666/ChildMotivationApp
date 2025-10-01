be4cfa1d607e98d3f96a170e0b0890a33760e590
Kwameldx666 <artemios.sologan@mib.utm.md>
2025-10-01 23:15:25 +0300

    Add project files.

---
+++ ChildMotivationApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ChildMotivationApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ChildMotivationApp.Pages")]
---
+++ ChildMotivationApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChildMotivationApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
