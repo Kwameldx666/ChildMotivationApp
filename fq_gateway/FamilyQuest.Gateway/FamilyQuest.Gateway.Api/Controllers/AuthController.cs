d1e4ab071e7baa17ad35887ee888f14782ae25a9
asologan <artemios.sologan@endava.com>
2025-12-17 18:55:19 +0200

    Implement initial authentication controller and project file setup

---
+++ fq_gateway/FamilyQuest.Gateway/FamilyQuest.Gateway.Api/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyQuest.Gateway.Controllers;
+
+[ApiController]
+[Route("family-quest/[controller]")]
+public class AuthController(ILogger<AuthController> logger) : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger = logger;
+}
