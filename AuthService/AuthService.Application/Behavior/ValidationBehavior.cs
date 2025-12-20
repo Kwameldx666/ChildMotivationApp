f0ea6cfed7efc3ad23067fc179ffd8945145a85f
Kwameldx666 <artemios.sologan@mib.utm.md>
2025-12-20 15:56:40 +0200

    feat: Add initial repository and error handling classes for authentication

---
+++ AuthService/AuthService.Application/Behavior/ValidationBehavior.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthService.Application.Behavior;
+
+public class ValidationBehavior
+{
+    
+}
---
+++ AuthService/AuthService.Common/Constants/Errors/AuthorizationErrors.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthService.Common.ResultPattern;
+
+public class AuthorizationErrors
+{
+    
+}
