fe4c465b8ff0daf282e67a1cc5ece6a96a304475
Kwameldx666 <artemios.sologan@mib.utm.md>
2025-12-23 17:51:42 +0200

    feat: add initial exception handling, unit of work, and job classes; remove obsolete migration files

---
+++ AuthService/AuthService.Api/Exceptions/ApiException.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthService.Exceptions;
+
+public class ApiException
+{
+    
+}
---
+++ AuthService/AuthService.Infrastructure/Services/Quartz/CleanRefreshTokenJob.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthService.Infrastructure.Services.Quartz;
+
+public class CleanRefreshTokenJob
+{
+    
+}
